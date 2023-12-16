d3cca1099a4eb29e9b7959a04168f3a7bb585056
Sergio <svleez@gmail.com>
2023-12-16 18:42:26 -0500

    Hotfix database url
    - Change the sqlite database url path to be on the project directory.

--- secret_santa/database.py
+++ secret_santa/database.py
@@ -1,4 +1,5 @@
 from os import getenv
+from pathlib import Path
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -8,9 +9,10 @@ from sqlalchemy.ext.declarative import declarative_base
 load_dotenv()
 
 # Environment variables
+PROJECT_DIR = Path(__file__).resolve().parent.parent
 DATABASE_NAME = getenv("DATABASE_NAME")
-# DATABASE_URL = f"sqlite:///{DATABASE_NAME}"
-DATABASE_URL = f"sqlite:////home/ubuntu/secret-santa/{DATABASE_NAME}"
+
+DATABASE_URL = f"sqlite:///{PROJECT_DIR / DATABASE_NAME}"
 
 # SQLAlchemy
 engine = create_engine(
